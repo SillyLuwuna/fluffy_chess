40dead0581458124cde27594844cf83a7736186e
luna <rokdoombeast@gmail.com>
2026-05-03 01:04:43 +0100

    cleaned up board_printer

--- include/board_printer.hpp
+++ include/board_printer.hpp
@@ -6,6 +6,15 @@ namespace fluffy_chess
 	class BoardPrinter
 	{
 		private:
+			constexpr static std::string get_fen_castling_rights(const Board& board);
+			constexpr static std::string get_fen_board(const Board& board);
+			constexpr static std::string get_fen_rank(const Board& board, int rank);
+			constexpr static char get_fen_piece_at(const Board& board, Bitboard pos);
+			constexpr static std::string get_fen_player(const Board& board);
+			constexpr static std::string get_fen_en_passant(const Board& board);
+			constexpr static std::string get_fen_hc(const Board& board);
+			constexpr static std::string get_fen_fc(const Board& board);
+
 		public:
 			static void print(const Board& board);
 			constexpr static std::string get_string(const Board& board);
@@ -27,95 +36,119 @@ constexpr std::string BoardPrinter::get_string(const Board& board)
 
 		for (int j = 0; j < 8; j++)
 		{
-			board_str += " ";
-			char c = piece_char(board.get_piece_at(gen_bitboard(i, j)));
-			board_str += board.get_color_at(gen_bitboard(i, j)) == White ? std::toupper(c) : c;
-			board_str += " |";
+			board_str += " " + std::to_string(get_fen_piece_at(board, gen_bitboard(i, j))) + " |";
 		}
 
 		board_str += "\n---------------------------------\n";
 	}
 
-	board_str += "player: ";
-	board_str += board.is_black_playing() ? "black\n" : "white\n";
-
-	board_str += "castling_rights: ";
-	board_str += board.white_can_castle_king_side() ? "K" : "";
-	board_str += board.white_can_castle_queen_side() ? "Q" : "";
-	board_str += board.black_can_castle_king_side() ? "k" : "";
-	board_str += board.black_can_castle_queen_side() ? "q" : "";
-	board_str += "\n";
-
-	board_str += "ep_passant: ";
-	Bitboard en_passant_square = board.get_en_passant_square();
-	board_str += en_passant_square != 0ULL ? bitboard_to_algebraic(en_passant_square) : "-";
-	board_str += "\n";
-
-	board_str += "half_clock: ";
-	board_str += std::to_string(board.get_hm_clock());
-	board_str += "\n";
-	board_str += "full_clock: ";
-	board_str += std::to_string(board.get_fm_clock());
-	board_str += "\n";
+	board_str += "player: " + get_fen_player(board) + "\n";
+	board_str += "castling_rights: " + get_fen_castling_rights(board) + "\n";
+	board_str += "ep_passant: " + get_fen_en_passant(board) + "\n";
+	board_str += "half_clock: " + get_fen_hc(board) + "\n";
+	board_str += "full_clock: " + get_fen_fc(board) + "\n";
 
 	return board_str;
 }
 
 constexpr std::string BoardPrinter::get_fen_string(const Board& board)
 {
-	// "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 	std::string fen_str;
+
+	fen_str += get_fen_board(board) + " ";
+	fen_str += get_fen_player(board) + " ";
+	fen_str += get_fen_castling_rights(board) + " ";
+	fen_str += get_fen_en_passant(board) + " ";
+	fen_str += get_fen_hc(board) + " ";
+	fen_str += get_fen_fc(board);
+
+	return fen_str;
+}
+
+constexpr std::string BoardPrinter::get_fen_board(const Board& board)
+{
+	std::string fen_str;
+	
 	for (int i = 7; i >= 0; i--)
 	{
-		uint8_t no_piece_counter = 0;
-		for (int j = 0; j < 8; j++)
-		{
-			Bitboard pos = gen_bitboard(i, j);
-			if (!board.has_piece_at(pos))
-			{
-				no_piece_counter++;
-				continue;
-			}
-
-			if(no_piece_counter > 0)
-			{
-				fen_str += no_piece_counter;
-			}
-
-			char c = piece_char(board.get_piece_at(pos));
-			fen_str += board.get_color_at(gen_bitboard(i, j)) == White ? std::toupper(c) : c;
-			no_piece_counter = 0;
-		}
-
-		if(no_piece_counter > 0)
-		{
-			fen_str += std::to_string(no_piece_counter);
-		}
+		fen_str += get_fen_rank(board, i);
 
 		if (i > 0)
 		{
 			fen_str += "/";
 		}
 	}
-	fen_str += " ";
 
-	fen_str += board.is_black_playing() ? "b" : "w";
-	fen_str += " ";
+	return fen_str;
+}
+
+constexpr std::string BoardPrinter::get_fen_rank(const Board& board, int rank)
+{
+	std::string fen_str;
+
+	uint8_t no_piece_counter = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		Bitboard pos = gen_bitboard(rank, i);
+
+		if (!board.has_piece_at(pos))
+		{
+			no_piece_counter++;
+			continue;
+		}
+
+		fen_str += no_piece_counter > 0 ? std::to_string(no_piece_counter) : "";
+		fen_str += get_fen_piece_at(board, pos);
+
+		no_piece_counter = 0;
+	}
+
+	fen_str += no_piece_counter > 0 ? std::to_string(no_piece_counter) : "";
+
+	return fen_str;
+}
+
+constexpr char BoardPrinter::get_fen_piece_at(const Board& board, Bitboard pos)
+{
+	char c = piece_char(board.get_piece_at(pos));
+	return board.get_color_at(pos) == White ? std::toupper(c) : c;
+}
+
+constexpr std::string BoardPrinter::get_fen_castling_rights(const Board& board)
+{
+	std::string fen_str;
 
 	fen_str += board.white_can_castle_king_side() ? "K" : "";
 	fen_str += board.white_can_castle_queen_side() ? "Q" : "";
 	fen_str += board.black_can_castle_king_side() ? "k" : "";
 	fen_str += board.black_can_castle_queen_side() ? "q" : "";
-	fen_str += " ";
 
-	Bitboard en_passant_square = board.get_en_passant_square();
-	fen_str += en_passant_square != 0ULL ? bitboard_to_algebraic(en_passant_square) : "-";
-	fen_str += " ";
-
-	fen_str += std::to_string(board.get_hm_clock());
-	fen_str += " ";
-
-	fen_str += std::to_string(board.get_fm_clock());
+	if (fen_str == "")
+	{
+		fen_str = "-";
+	}
 
 	return fen_str;
 }
+
+constexpr std::string BoardPrinter::get_fen_player(const Board& board)
+{
+	return board.is_black_playing() ? "b" : "w";
+}
+
+constexpr std::string BoardPrinter::get_fen_en_passant(const Board& board)
+{
+	Bitboard en_passant_square = board.get_en_passant_square();
+	return en_passant_square != 0ULL ? bitboard_to_algebraic(en_passant_square) : "-";
+}
+
+constexpr std::string BoardPrinter::get_fen_hc(const Board& board)
+{
+	return std::to_string(board.get_hm_clock());
+}
+
+constexpr std::string BoardPrinter::get_fen_fc(const Board& board)
+{
+	return std::to_string(board.get_fm_clock());
+}
+
